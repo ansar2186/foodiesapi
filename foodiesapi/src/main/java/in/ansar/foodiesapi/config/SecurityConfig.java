df2cdd710c141e9bd454f6367bbb17b5a2f8a027
Ansar Ahmad <ansar.ahmad@dxc.com>
2025-05-05 16:04:52 +0530

    addt the admin and clinet url in SecurityConfig file

--- foodiesapi/src/main/java/in/ansar/foodiesapi/config/SecurityConfig.java
+++ foodiesapi/src/main/java/in/ansar/foodiesapi/config/SecurityConfig.java
@@ -59,7 +59,7 @@ public class SecurityConfig {
 
 	private UrlBasedCorsConfigurationSource corsConfigurationSource() {
 		CorsConfiguration config = new CorsConfiguration();
-		config.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:5174"));
+		config.setAllowedOrigins(List.of("https://rainbow-alfajores-5bbb3c.netlify.app/", "https://dulcet-lollipop-54a5b4.netlify.app/"));
 		config.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
 		config.setAllowedHeaders(List.of("Authorization", "Content-Type"));
 		config.setAllowCredentials(true);
